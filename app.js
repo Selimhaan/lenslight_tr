b293e23325315c2f949aaa636294b03f569f8340
Selimhaan <saltinpinar8@gmail.com>
2025-01-12 11:46:01 +0300

    gün 3

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conn from "./db.js";
+import pageRoute from './routes/pageRoute.js';
+import photoRoute from './routes/photoRoute.js';
 
 dotenv.config();
 
@@ -17,13 +19,18 @@ app.set('view engine', 'ejs');
 //static files middleware
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+
+// routes
+app.use('/', pageRoute);
+app.use('/photos', photoRoute);
+
+/* app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
-});
+}); */
 
 app.listen(port, () => {
     console.log(`Application running on port ${port}`);

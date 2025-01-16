3572dc0a77d860d3138fe48a5fc3b1324a6c6dfe
Selimhaan <saltinpinar8@gmail.com>
2025-01-16 13:32:21 +0300

    gün 7

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import conn from "./db.js";
 import pageRoute from './routes/pageRoute.js';
 import photoRoute from './routes/photoRoute.js';
 import userRoute from './routes/userRoute.js';
+import cookieParser from 'cookie-parser';
+import { checkUser } from './middlewares/authMiddleware.js';
 
 dotenv.config();
 
@@ -21,9 +23,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 
 
 // routes
+app.get('*', checkUser);
 app.use('/', pageRoute);
 app.use('/photos', photoRoute);
 app.use('/users', userRoute);
